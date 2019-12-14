26bdf4e81963437894083a3c539fb26e46828682
joanna-gabis <joanna.gabis@gmail.com>
2019-12-14 16:49:06 +0100

    Filter profane words out, prevent multiple form submits

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,35 +1,47 @@
 const socket = io();
 
-// socket.on('countUpdated', count => {
-//   console.log('The count has been updated', count);
-// });
-
-// document.getElementById('increment').addEventListener('click', () => {
-//   console.log('clicked');
-//   socket.emit('increment');
-// });
+const messageForm = document.getElementById('messageForm');
+const messageInput = document.getElementById('messageInput');
+const submitMessageButton = messageForm.querySelector('button[type="submit"]');
+const sendLocationButton = document.getElementById('sendLocation');
 
 socket.on('renderMessage', message => console.log(message));
 
-document.querySelector('form').addEventListener('submit', e => {
+messageForm.addEventListener('submit', e => {
     e.preventDefault();
+    submitMessageButton.setAttribute('disabled', 'disabled');
     let message = e.target.elements.message.value;
-    socket.emit('sendMessage', message, () =>
-        console.log('The message was delivered')
-    );
+    socket.emit('sendMessage', message, error => {
+        submitMessageButton.removeAttribute('disabled');
+        if (error) {
+            console.log(error);
+        } else {
+            messageInput.value = '';
+            console.log('The message was delivered');
+        }
+    });
 });
 
-document.getElementById('sendLocation').addEventListener('click', () => {
+sendLocationButton.addEventListener('click', () => {
     if (!navigator.geolocation) {
         return alert('Your browser does not support geolocation');
     }
 
+    sendLocationButton.setAttribute('disabled', 'disabled');
+
     navigator.geolocation.getCurrentPosition(
         position => {
-            socket.emit('sendLocation', {
-                lat: position.coords.latitude,
-                lon: position.coords.longitude
-            });
+            socket.emit(
+                'sendLocation',
+                {
+                    lat: position.coords.latitude,
+                    lon: position.coords.longitude
+                },
+                () => {
+                    sendLocationButton.removeAttribute('disabled');
+                    console.log('Location shared');
+                }
+            );
         },
         e => console.log(e),
         {
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const http = require('http');
 const path = require('path');
 const express = require('express');
 const socketio = require('socket.io');
+const Filter = require('bad-words');
 
 const app = express();
 const server = http.createServer(app);
@@ -18,19 +19,25 @@ io.on('connection', socket => {
     socket.emit('renderMessage', 'welcome');
     socket.broadcast.emit('renderMessage', 'New user has joined');
 
-    socket.on('sendMessage', messageToRender => {
+    socket.on('sendMessage', (messageToRender, callback) => {
+        const filter = new Filter();
+        if (filter.isProfane(messageToRender)) {
+            return callback('Profanity is not allowed');
+        }
         io.emit('renderMessage', messageToRender);
+        callback();
     });
 
     socket.on('disconnect', () => {
         io.emit('renderMessage', 'user has left');
     });
 
-    socket.on('sendLocation', positionObject => {
+    socket.on('sendLocation', (positionObject, callback) => {
         socket.broadcast.emit(
             'renderMessage',
             `https://google.com/maps?q=${positionObject.lat},${positionObject.lon}`
         );
+        callback();
     });
 });
 
